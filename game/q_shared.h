ed66ca129375ec73e6c42387feeda47cedffd0b3
Michael Eggers <mseggers@protonmail.ch>
2023-04-22 16:25:27 +0200

    Allow for long file paths in map file

--- game/q_shared.h
+++ game/q_shared.h
@@ -67,7 +67,7 @@ typedef enum {false, true}	qboolean;
 
 #define	MAX_STRING_CHARS	1024	// max length of a string passed to Cmd_TokenizeString
 #define	MAX_STRING_TOKENS	80		// max tokens resulting from Cmd_TokenizeString
-#define	MAX_TOKEN_CHARS		128		// max length of an individual token
+#define	MAX_TOKEN_CHARS		1024    // max length of an individual token
 
 #define	MAX_QPATH			64		// max length of a quake game pathname
 #define	MAX_OSPATH			128		// max length of a filesystem pathname
